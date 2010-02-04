302384e9e0ac1b412ed4293a7133c1da001526ad
Remco Vermeulen <rvermeulen@gmail.com>
2010-02-04 20:30:47 +0100

    Added module export parsing

--- target-i386/argos-tracksc.c
+++ target-i386/argos-tracksc.c
@@ -1,5 +1,7 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <wchar.h>
 
 #include "cpu.h"
 #include "argos-config.h"
@@ -7,12 +9,17 @@
 #include "argos-check.h"
 #include "../exec-all.h"
 #include "libdasm/libdasm.h"
+#include "win2k/winternl.h"
 #include "argos-tracksc.h"
+
 
 static INSTRUCTION current_instr;
 // The pc must be a host virtual address.
 static int argos_tracksc_instr_len(unsigned long pc);
 static void argos_tracksc_dump_current_instr(argos_shellcode_context_t * context);
+static void argos_tracksc_dump_loaded_modules(CPUX86State * env);
+
+#define PHYS_ADDR(X) (get_phys_addr_code(env, (target_ulong)(X)) + (unsigned long) phys_ram_base)
 
 int argos_tracksc_instr_len(unsigned long pc)
 {
@@ -22,7 +29,8 @@ int argos_tracksc_instr_len(unsigned long pc)
 void argos_tracksc_dump_current_instr(argos_shellcode_context_t * context)
 {
     char instr_str[1024];
-    if (get_instruction_string(&current_instr, FORMAT_INTEL, 0, instr_str, sizeof(instr_str)))
+    if (get_instruction_string(&current_instr, FORMAT_INTEL, 0, instr_str, 
+                sizeof(instr_str)))
     {
         fprintf(context->logfile, "%s", instr_str);
     }
@@ -72,6 +80,9 @@ int argos_tracksc_is_running(CPUX86State * env)
 
 void argos_tracksc_enable(CPUX86State * env)
 {
+    argos_logf("Dumping loaded modules for targeted process.\n");
+    argos_tracksc_dump_loaded_modules(env);
+
     argos_logf("Starting shell-code tracking...\n");
     env->shellcode_context.running = 1;
     env->shellcode_context.cr3 = env->cr[3];
@@ -108,21 +119,21 @@ void argos_tracksc_store_context(CPUX86State * env)
         unsigned max_stage = 0;
 #endif // ARGOS_NET_TRACKER
 
-        // hph = host physical, gv = guest virtual, 
+        // hp = host physical, gv = guest virtual, 
         // and correspond to a memory address.
-        unsigned long hph_pc = 0, gv_pc = 0;
+        unsigned long hp_pc = 0, gv_pc = 0;
 
         gv_pc = env->segs[R_CS].base + env->eip;
         // Casting the phys_ram_base to target_ulong will
         // trim the address from 64 bits to 32 bits and yields
         // a wrong address when executing Argos on an amd64 host.
-        hph_pc = get_phys_addr_code(env, gv_pc) +
+        hp_pc = get_phys_addr_code(env, gv_pc) +
             (unsigned long)phys_ram_base;
 
         //env->shellcode_context.instruction_size = env->current_tb->size;
-        env->shellcode_context.instruction_size = argos_tracksc_instr_len(hph_pc);
+        env->shellcode_context.instruction_size = argos_tracksc_instr_len(hp_pc);
 
-        memcpy(env->shellcode_context.instruction, (void *)(unsigned long)hph_pc,
+        memcpy(env->shellcode_context.instruction, (void *)hp_pc,
                 env->shellcode_context.instruction_size);
 
         env->shellcode_context.executed_eip = env->eip;
@@ -135,7 +146,7 @@ void argos_tracksc_store_context(CPUX86State * env)
         for ( i = 0; i < env->shellcode_context.instruction_size;
                 i++ )
         {
-            argos_netidx_t* netidx = ARGOS_NETIDXPTR(hph_pc + i);
+            argos_netidx_t* netidx = ARGOS_NETIDXPTR(hp_pc + i);
             if (netidx != 0)
             {
                 // Currently the stage of the instruction equals 
@@ -407,4 +418,102 @@ int argos_tracksc_log_system_call(CPUX86State * env)
 int argos_tracksc_logged_system_call(CPUX86State * env)
 {
     return env->shellcode_context.is_system_call;
+}
+
+static void argos_tracksc_dump_loaded_modules(CPUX86State * env)
+{
+    target_ulong gv_fs = 0;
+    unsigned long hp_fs = 0;
+    PTEB teb = 0;
+    PPEB peb = 0;
+    PPEB_LDR_DATA ldr_data = 0;
+    LIST_ENTRY module_list;
+    PLIST_ENTRY forward_iterator = 0;
+    PLDR_DATA_TABLE_ENTRY module_table = 0;
+    unsigned char first_module = 1;
+
+    // fs:[0x0] contains the Thread Execution Block.
+    gv_fs = env->segs[R_FS].base;
+    hp_fs = PHYS_ADDR(gv_fs);
+
+    argos_logf("fs:[0x0] = 0x%x  @ 0x%lx\n", gv_fs, hp_fs);
+
+    teb = (PTEB)hp_fs;
+    argos_logf("peb = 0x%x\n", teb->Peb);
+    peb = (PPEB)PHYS_ADDR(teb->Peb);
+    argos_logf("peb @ 0x%lx\n", peb);
+
+    argos_logf("peb->LoaderData = 0x%x", peb->LoaderData);
+    ldr_data = (PPEB_LDR_DATA) PHYS_ADDR(peb->LoaderData);
+    argos_logf("peb->LoaderData @ 0x%lx", ldr_data);
+
+    module_list = ldr_data->InLoadOrderModuleList;
+    argos_logf("peb->LoaderData->InLoadOrderModuleList.Flink = 0x%x\n",
+            module_list.Flink);
+    forward_iterator = (PLIST_ENTRY) PHYS_ADDR(module_list.Flink);
+    argos_logf("Forward entry = 0x%x @ 0x%lx\n", module_list.Flink,
+            forward_iterator);
+
+    module_table = (PLDR_DATA_TABLE_ENTRY)forward_iterator;
+    while ( module_table != 0 )
+    {
+        printf("Module base: 0x%x\n", module_table->BaseAddress);
+        if ( !first_module )
+        {
+            PIMAGE_DOS_HEADER dos_hdr = (PIMAGE_DOS_HEADER) PHYS_ADDR(module_table->BaseAddress);
+
+            if ( dos_hdr->e_magic == IMAGE_DOS_SIGNATURE  )
+            {
+                PIMAGE_NT_HEADERS pe_hdr =
+                    (PIMAGE_NT_HEADERS)PHYS_ADDR(module_table->BaseAddress + dos_hdr->e_lfanew);
+
+                if ( pe_hdr->Signature == IMAGE_PE_SIGNATURE )
+                {
+                    PIMAGE_DATA_DIRECTORY export_data_dir = 0;
+                    argos_logf("Found valid pe signature!\n");
+
+                    if ( pe_hdr->OptionalHeader.NumberOfRvaAndSizes > 0 )
+                    {
+                        export_data_dir = &pe_hdr->OptionalHeader.DataDirectory[0];
+                        if ( export_data_dir->VirtualAddress != 0 )
+                        {
+                            PIMAGE_EXPORT_DIRECTORY export_dir =
+                                (PIMAGE_EXPORT_DIRECTORY)
+                                PHYS_ADDR(module_table->BaseAddress +
+                                        export_data_dir->VirtualAddress);
+
+                            char * name = (char*)PHYS_ADDR(module_table->BaseAddress + 
+                                    export_dir->Name);
+                            printf("Found module %s\n", name);
+                        }
+                        else
+                        {
+                            printf("Module has no export table!\n");
+                        }
+                    }
+                }
+                else
+                {
+                    argos_logf("Found invalid pe signature!\n");
+                }
+            }
+            else
+            {
+                argos_logf("Found invalid dos signature!\n");
+            }
+        }
+        else
+        {
+            first_module = 0;
+        }
+
+        if ( forward_iterator->Flink == module_list.Blink )
+        {
+            break;
+        }
+
+
+        forward_iterator = (PLIST_ENTRY) PHYS_ADDR(forward_iterator->Flink);
+        module_table = (PLDR_DATA_TABLE_ENTRY)forward_iterator;
+    }
 }
---
+++ target-i386/win2k/winternl.h
@@ -0,0 +1,395 @@
+#ifndef _WINITERNL_H
+#define _WINITERNL_H
+
+#include <stdint.h>
+
+typedef uint32_t PTR;
+
+//typedef unsigned char BYTE;
+typedef int16_t SHORT;
+typedef uint16_t USHORT;
+//typedef unsigned short WORD;
+typedef int32_t LONG;
+typedef uint32_t ULONG;
+typedef int64_t LONGLONG;
+typedef uint64_t ULONGLONG;
+//typedef unsigned long DWORD;
+typedef uint8_t CHAR;
+typedef CHAR* PCHAR;
+typedef uint16_t WCHAR;
+typedef WCHAR* PWSTR;
+typedef uint8_t BOOLEAN;
+typedef void VOID;
+//typedef VOID* PVOID;
+typedef PTR PVOID;
+//typedef PVOID* PPVOID;
+typedef PTR PPVOID;
+typedef LONG HANDLE;
+typedef LONG NTSTATUS;
+//typedef void* PPS_POST_PROCESS_INIT_ROUTINE;
+typedef PTR PPS_POST_PROCESS_INIT_ROUTINE;
+typedef void (*PPEBLOCKROUTINE)(PVOID PebLock); 
+
+typedef union _LARGE_INTEGER {
+    struct {
+        DWORD LowPart;
+        LONG HighPart;
+    };
+
+    struct {
+        DWORD LowPart;
+        LONG HighPart;
+    } u;
+    LONGLONG QuadPart;
+} LARGE_INTEGER, *PLARGE_INTEGER __attribute__((aligned (4)));
+
+typedef struct _LIST_ENTRY {
+    //struct _LIST_ENTRY *Flink;
+    PVOID Flink;
+    //struct _LIST_ENTRY *Blink;
+    PVOID Blink;
+} LIST_ENTRY, *PLIST_ENTRY __attribute__((aligned (4)));
+
+typedef struct _STRING {
+    USHORT  Length;
+    USHORT  MaximumLength;
+    //PCHAR  Buffer;
+    PVOID Buffer;
+} ANSI_STRING, *PANSI_STRING __attribute__((aligned (4)));
+
+typedef struct _LSA_UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    //PWSTR  Buffer;
+    PVOID Buffer;
+}LSA_UNICODE_STRING, *PLSA_UNICODE_STRING, UNICODE_STRING, *PUNICODE_STRING __attribute__((aligned (4)));
+
+typedef struct _PEB_LDR_DATA {
+    ULONG Length; 
+    BOOLEAN Initialized; 
+    PVOID SsHandle; 
+    LIST_ENTRY InLoadOrderModuleList; 
+    LIST_ENTRY InMemoryOrderModuleList; 
+    LIST_ENTRY InInitializationOrderModuleList;
+} PEB_LDR_DATA, *PPEB_LDR_DATA __attribute__((aligned (4)));
+
+typedef struct _LDR_DATA_TABLE_ENTRY {
+    LIST_ENTRY InLoadOrderModuleList; 
+    LIST_ENTRY InMemoryOrderModuleList; 
+    LIST_ENTRY InInitializationOrderModuleList; 
+    PVOID BaseAddress; 
+    PVOID EntryPoint; 
+    ULONG SizeOfImage; 
+    UNICODE_STRING FullDllName; 
+    UNICODE_STRING BaseDllName; 
+    ULONG Flags; 
+    SHORT LoadCount; 
+    SHORT TlsIndex; 
+    LIST_ENTRY HashTableEntry;
+    ULONG TimeDateStamp;
+} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY __attribute__((aligned (4)));
+
+typedef struct _RTL_DRIVE_LETTER_CURDIR {
+    USHORT Flags; 
+    USHORT Length; 
+    ULONG TimeStamp;
+    UNICODE_STRING DosPath;
+} RTL_DRIVE_LETTER_CURDIR, *PRTL_DRIVE_LETTER_CURDIR __attribute__((aligned (4)));
+
+typedef struct _RTL_USER_PROCESS_PARAMETERS {
+    ULONG MaximumLength; 
+    ULONG Length; 
+    ULONG Flags; 
+    ULONG DebugFlags; 
+    PVOID ConsoleHandle; 
+    ULONG ConsoleFlags; 
+    HANDLE StdInputHandle; 
+    HANDLE StdOutputHandle; 
+    HANDLE StdErrorHandle; 
+    UNICODE_STRING CurrentDirectoryPath; 
+    HANDLE CurrentDirectoryHandle; 
+    UNICODE_STRING DllPath; 
+    UNICODE_STRING ImagePathName; 
+    UNICODE_STRING CommandLine; 
+    PVOID Environment; 
+    ULONG StartingPositionLeft; 
+    ULONG StartingPositionTop; 
+    ULONG Width; 
+    ULONG Height; 
+    ULONG CharWidth; 
+    ULONG CharHeight; 
+    ULONG ConsoleTextAttributes; 
+    ULONG WindowFlags; 
+    ULONG ShowWindowFlags; 
+    UNICODE_STRING WindowTitle; 
+    UNICODE_STRING DesktopName;
+    UNICODE_STRING ShellInfo; 
+    UNICODE_STRING RuntimeData; 
+    RTL_DRIVE_LETTER_CURDIR DLCurrentDirectory[0x20];
+} RTL_USER_PROCESS_PARAMETERS, *PRTL_USER_PROCESS_PARAMETERS __attribute__((aligned (4)));
+
+typedef struct _PEB_FREE_BLOCK {
+    //struct _PEB_FREE_BLOCK *Next;
+    PVOID Next;
+    ULONG Size;
+} PEB_FREE_BLOCK, *PPEB_FREE_BLOCK __attribute__((aligned (4)));
+
+typedef struct _PEB {
+    BOOLEAN InheritedAddressSpace; 
+    BOOLEAN ReadImageFileExecOptions; 
+    BOOLEAN BeingDebugged; 
+    BOOLEAN Spare; 
+    HANDLE Mutant; 
+    PVOID ImageBaseAddress; 
+    //PPEB_LDR_DATA LoaderData;
+    PVOID LoaderData;
+    //PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
+    PVOID ProcessParameters;
+    PVOID SubSystemData; 
+    PVOID ProcessHeap; 
+    PVOID FastPebLock; 
+    //PPEBLOCKROUTINE FastPebLockRoutine; 
+    PVOID FastPebLockRoutine;
+    //PPEBLOCKROUTINE FastPebUnlockRoutine;
+    PVOID FastPebUnlockRoutine;
+    ULONG EnvironmentUpdateCount; 
+    PPVOID KernelCallbackTable; 
+    PVOID EventLogSection; 
+    PVOID EventLog; 
+    //PPEB_FREE_BLOCK FreeList;
+    PVOID FreeList;
+    ULONG TlsExpansionCounter; 
+    PVOID TlsBitmap; 
+    ULONG TlsBitmapBits[0x2]; 
+    PVOID ReadOnlySharedMemoryBase; 
+    PVOID ReadOnlySharedMemoryHeap; 
+    PPVOID ReadOnlyStaticServerData; 
+    PVOID AnsiCodePageData; 
+    PVOID OemCodePageData; 
+    PVOID UnicodeCaseTableData; 
+    ULONG NumberOfProcessors; 
+    ULONG NtGlobalFlag; 
+    BYTE Spare2[0x4]; 
+    LARGE_INTEGER CriticalSectionTimeout; 
+    ULONG HeapSegmentReserve; 
+    ULONG HeapSegmentCommit; 
+    ULONG HeapDeCommitTotalFreeThreshold; 
+    ULONG HeapDeCommitFreeBlockThreshold; 
+    ULONG NumberOfHeaps; 
+    ULONG MaximumNumberOfHeaps; 
+    PPVOID ProcessHeaps; 
+    PVOID GdiSharedHandleTable; 
+    PVOID ProcessStarterHelper; 
+    PVOID GdiDCAttributeList; 
+    PVOID LoaderLock; 
+    ULONG OSMajorVersion; 
+    ULONG OSMinorVersion; 
+    ULONG OSBuildNumber; 
+    ULONG OSPlatformId; 
+    ULONG ImageSubSystem; 
+    ULONG ImageSubSystemMajorVersion; 
+    ULONG ImageSubSystemMinorVersion; 
+    ULONG GdiHandleBuffer[0x22]; 
+    ULONG PostProcessInitRoutine; 
+    ULONG TlsExpansionBitmap; 
+    BYTE TlsExpansionBitmapBits[0x80]; 
+    ULONG SessionId;
+} PEB, *PPEB __attribute__((aligned (4)));
+
+typedef struct _EXCEPTION_RECORD
+{
+    LONG ExceptionCode;
+    ULONG ExceptionFlags;
+    //struct _EXCEPTION_RECORD* ExceptionRecord;
+    PVOID ExceptionRecord;
+    PVOID ExceptionAddress;
+    ULONG NumberParameters;
+    ULONG ExceptionInformation[15];
+} EXCEPTION_RECORD, *PEXCEPTION_RECORD __attribute__((aligned (4)));
+
+typedef struct _EXCEPTION_REGISTRATION_RECORD {
+    //struct _EXCEPTION_REGISTRATION_RECORD* Next;
+    PVOID Next;
+    PVOID Handler;
+} EXCEPTION_REGISTRATION_RECORD, *PEXCEPTION_REGISTRATION_RECORD __attribute__((aligned (4)));
+
+typedef struct _NT_TIB
+{
+    //PEXCEPTION_REGISTRATION_RECORD ExceptionList;
+    PVOID ExceptionList;
+    PVOID StackBase;
+    PVOID StackLimit;
+    PVOID SubSystemTib;
+    union
+    {
+        PVOID FiberData;
+        ULONG Version;
+    };
+    PVOID ArbitraryUserPointer;
+    //struct _NT_TIB* Self;
+    PVOID Self;
+} NT_TIB, *PNT_TIB __attribute__((aligned (4)));
+
+typedef struct _CLIENT_ID
+{
+    PVOID UniqueProcess;
+    PVOID UniqueThread;
+} CLIENT_ID, *PCLIENT_ID __attribute__((aligned (4)));
+
+typedef struct _TEB {
+    NT_TIB Tib; 
+    PVOID EnvironmentPointer; 
+    CLIENT_ID Cid; 
+    PVOID ActiveRpcInfo; 
+    PVOID ThreadLocalStoragePointer; 
+    //PPEB Peb;
+    PVOID Peb;
+    ULONG LastErrorValue; 
+    ULONG CountOfOwnedCriticalSections; 
+    PVOID CsrClientThread; 
+    PVOID Win32ThreadInfo; 
+    ULONG Win32ClientInfo[0x1F]; 
+    PVOID WOW32Reserved; 
+    ULONG CurrentLocale; 
+    ULONG FpSoftwareStatusRegister; 
+    PVOID SystemReserved1[0x36]; 
+    PVOID Spare1; 
+    ULONG ExceptionCode; 
+    ULONG SpareBytes1[0x28]; 
+    PVOID SystemReserved2[0xA]; 
+    ULONG GdiRgn; 
+    ULONG GdiPen; 
+    ULONG GdiBrush; 
+    CLIENT_ID RealClientId; 
+    PVOID GdiCachedProcessHandle; 
+    ULONG GdiClientPID; 
+    ULONG GdiClientTID; 
+    PVOID GdiThreadLocaleInfo; 
+    PVOID UserReserved[5]; 
+    PVOID GlDispatchTable[0x118]; 
+    ULONG GlReserved1[0x1A]; 
+    PVOID GlReserved2; 
+    PVOID GlSectionInfo; 
+    PVOID GlSection; 
+    PVOID GlTable; 
+    PVOID GlCurrentRC; 
+    PVOID GlContext; 
+    NTSTATUS LastStatusValue; 
+    UNICODE_STRING StaticUnicodeString; 
+    WCHAR StaticUnicodeBuffer[0x105]; 
+    PVOID DeallocationStack; 
+    PVOID TlsSlots[0x40]; 
+    LIST_ENTRY TlsLinks; 
+    PVOID Vdm; 
+    PVOID ReservedForNtRpc; 
+    PVOID DbgSsReserved[0x2]; 
+    ULONG HardErrorDisabled; 
+    PVOID Instrumentation[0x10]; 
+    PVOID WinSockData; 
+    ULONG GdiBatchCount; 
+    ULONG Spare2; 
+    ULONG Spare3; 
+    ULONG Spare4; 
+    PVOID ReservedForOle; 
+    ULONG WaitingOnLoaderLock; 
+    PVOID StackCommit; 
+    PVOID StackCommitMax; 
+    PVOID StackReserved;
+} TEB, *PTEB __attribute__((aligned (4)));
+
+#define IMAGE_DOS_SIGNATURE 0x5A4D
+typedef struct _IMAGE_DOS_HEADER
+{
+    WORD e_magic;
+    WORD e_cblp;
+    WORD e_cp;
+    WORD e_crlc;
+    WORD e_cparhdr;
+    WORD e_minalloc;
+    WORD e_maxalloc;
+    WORD e_ss;
+    WORD e_sp;
+    WORD e_csum;
+    WORD e_ip;
+    WORD e_cs;
+    WORD e_lfarlc;
+    WORD e_ovno;
+    WORD e_res[4];
+    WORD e_oemid;
+    WORD e_oeminfo;
+    WORD e_res2[10];
+    LONG e_lfanew;
+} IMAGE_DOS_HEADER, *PIMAGE_DOS_HEADER __attribute__((aligned (4)));
+
+typedef struct _IMAGE_FILE_HEADER {
+    WORD  Machine;
+    WORD  NumberOfSections;
+    DWORD TimeDateStamp;
+    DWORD PointerToSymbolTable;
+    DWORD NumberOfSymbols;
+    WORD  SizeOfOptionalHeader;
+    WORD  Characteristics;
+} IMAGE_FILE_HEADER, *PIMAGE_FILE_HEADER __attribute__((aligned (4)));
+
+typedef struct _IMAGE_DATA_DIRECTORY {
+      DWORD VirtualAddress;
+        DWORD Size;
+} IMAGE_DATA_DIRECTORY, *PIMAGE_DATA_DIRECTORY __attribute__((aligned (4)));
+
+#define IMAGE_NUMBEROF_DIRECTORY_ENTRIES 16
+typedef struct _IMAGE_OPTIONAL_HEADER {
+    WORD                 Magic;
+    BYTE                 MajorLinkerVersion;
+    BYTE                 MinorLinkerVersion;
+    DWORD                SizeOfCode;
+    DWORD                SizeOfInitializedData;
+    DWORD                SizeOfUninitializedData;
+    DWORD                AddressOfEntryPoint;
+    DWORD                BaseOfCode;
+    DWORD                BaseOfData;
+    DWORD                ImageBase;
+    DWORD                SectionAlignment;
+    DWORD                FileAlignment;
+    WORD                 MajorOperatingSystemVersion;
+    WORD                 MinorOperatingSystemVersion;
+    WORD                 MajorImageVersion;
+    WORD                 MinorImageVersion;
+    WORD                 MajorSubsystemVersion;
+    WORD                 MinorSubsystemVersion;
+    DWORD                Win32VersionValue;
+    DWORD                SizeOfImage;
+    DWORD                SizeOfHeaders;
+    DWORD                CheckSum;
+    WORD                 Subsystem;
+    WORD                 DllCharacteristics;
+    DWORD                SizeOfStackReserve;
+    DWORD                SizeOfStackCommit;
+    DWORD                SizeOfHeapReserve;
+    DWORD                SizeOfHeapCommit;
+    DWORD                LoaderFlags;
+    DWORD                NumberOfRvaAndSizes;
+    IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+} IMAGE_OPTIONAL_HEADER, *PIMAGE_OPTIONAL_HEADER __attribute__((aligned (4)));
+
+#define IMAGE_PE_SIGNATURE 0x00004550
+typedef struct _IMAGE_NT_HEADERS {
+    DWORD                 Signature;
+    IMAGE_FILE_HEADER     FileHeader;
+    IMAGE_OPTIONAL_HEADER OptionalHeader;
+} IMAGE_NT_HEADERS, *PIMAGE_NT_HEADERS __attribute__((aligned (4)));
+
+typedef struct _IMAGE_EXPORT_DIRECTORY {
+    DWORD Characteristics;
+    DWORD TimeDateStamp;
+    WORD MajorVersion;
+    WORD MinorVersion;
+    DWORD Name;
+    DWORD Base;
+    DWORD NumberOfFunctions;
+    DWORD NumberOfNames;
+    DWORD AdressOfFunctions;
+    DWORD AddressOfNames;
+    DWORD AddressOfNameOrdinals;
+} IMAGE_EXPORT_DIRECTORY, *PIMAGE_EXPORT_DIRECTORY  __attribute__((aligned (4)));
+
+#endif
